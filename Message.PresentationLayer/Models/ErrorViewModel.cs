2b5f3e6be2e0e620709cd0ec7026d49e371c49eb
tuba balkan <tubabalkan165@gmail.com>
2024-05-03 15:27:46 +0300

    Add project files.

---
+++ Message.PresentationLayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Message.PresentationLayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
