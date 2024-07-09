3daaa5612b909917ef352267f1f0fb368f645bc1
tuba balkan <tubabalkan165@gmail.com>
2024-07-09 19:01:07 +0300

    Tema Eklendi

---
+++ Message.PresentationLayer/Controllers/MessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Message.PresentationLayer.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult MessageList()
+        {
+            return View();
+        }
+    }
+}
